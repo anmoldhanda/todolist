1ed586ef159a981977d49d920505126d21004647
anmol dhanda <rk352714@gmail.com>
2024-05-03 00:37:40 +0530

    added custom alert & confirm box for checking empty inputfields & deleting & cancelling todoitem

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const todolist_notification = document.getElementById("todolist-notification");
 let updated_todotext = null;
 // ============================================ editing mode is initially set as false which means the user's isn't editing it's existing todo item ============================================
 let editing_mode_todotext = false;
+const todoitem_deletion_confirmationbox = document.getElementById("todoitem-deletion-confirmationbox");
+const cancel_deletionbtn = document.getElementById("cancel-deletionbtn");
+const yes_deletionbtn = document.getElementById("yes-deletionbtn");
+const overlay_notification_indicatortext = document.querySelectorAll(".overlay-notification-indicatortext");
+const emptyinput_alertbox = document.getElementById("emptyinput-alertbox");
+const closebtn_emptyinput_alertbox = document.getElementById("closebtn-emptyinput-alertbox");
 
 // ============================================ create new todo ============================================
 function create_todo() {
@@ -14,17 +20,23 @@ function create_todo() {
     li_tag.setAttribute("ondblclick","complete_todo(this)");
     li_tag.className = "each-todolist-itembox";
     li_tag.innerHTML = `<p class="todolist-itemtext"></p>
-        <div class="iconsdiv">
-            <i class="fa-solid fa-pencil" title="Edit todo" onclick="update_todo(this)" id="edit-todoitem"></i>
-            <i class="fa-solid fa-trash" title="Delete todo" onclick="delete_todo(this)" id="delete-todoitem"></i>
-        </div>`;
+    <div class="iconsdiv">
+    <i class="fa-solid fa-pencil" title="Edit todo" onclick="update_todo(this)" id="edit-todoitem"></i>
+    <i class="fa-solid fa-trash" title="Delete todo" onclick="delete_todo(this)" id="delete-todoitem"></i>
+    </div>`;
     let todolist_itemtext = li_tag.querySelector(".todolist-itemtext");
     todolist_itemtext.innerText = todolist_inputfield.value;
     todolist_itemsul.appendChild(li_tag);
-    todolist_inputfield.value = "";
     show_todolist_notification("new task added successfully");
+    todolist_inputfield.value = "";
   } else {
-    alert("enter the value to the form first");
+    emptyinput_alertbox.classList.add("showhide");
+    overlay_notification_indicatortext.forEach((each_overlay_notification_indicatortext)=>{
+      each_overlay_notification_indicatortext.innerText = "enter the value for the todo item to submit";
+    })
+    closebtn_emptyinput_alertbox.onclick = function() {
+      emptyinput_alertbox.classList.remove("showhide");
+    }
     todolist_inputfield.focus();
   }
 }
@@ -57,22 +69,32 @@ function update_todo(e) {
       }
     }
     
-    async function updatetodo_itemtext() {
+function updatetodo_itemtext() {
       // ============================================ check if the updated_todotext exists in our enviroment then edit the particular todo task by updating it & then reset the inputfield's value & again make sure the updated_todotext isn't existing in our enviroment ============================================
       if(updated_todotext !== null && editing_mode_todotext === true) {
         updated_todotext.innerText = todolist_inputfield.value;
         create_todo_btn.setAttribute("onclick","create_todo()");
         todolist_inputfield.value = "";
-      updated_todotext = null;
-      editing_mode_todotext = false;
+        updated_todotext = null;
+        editing_mode_todotext = false;
       show_todolist_notification("task updated successfully");
   }
 }
 
 // ============================================ delete todo ============================================
 function delete_todo(e) {
-  if(confirm(`are you sure that you want to delete the ${e.parentElement.previousElementSibling.innerText}`)) {
+  overlay_notification_indicatortext.forEach((each_overlay_notification_indicatortext)=>{
+    each_overlay_notification_indicatortext.innerHTML = `are you sure that you want to delete the ${e.parentElement.previousElementSibling.innerText}`;
+  })
+  // ============================================ if clicked on cancel delete btn then just hide the overlay notification popup box ============================================ 
+  todoitem_deletion_confirmationbox.classList.add("showhide");
+  cancel_deletionbtn.onclick = function() {
+    todoitem_deletion_confirmationbox.classList.remove("showhide");
+  }
+  // ============================================ if clicked on delete btn then hide the overlay notification popup box & delete the particular whole litag of todoitem ============================================ 
+  yes_deletionbtn.onclick = function() {
     e.parentElement.parentElement.remove();
+    todoitem_deletion_confirmationbox.classList.remove("showhide");
   }
 }
 
